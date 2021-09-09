a8c36d4499c2f38717b27f06a462e3a6ee785897
vineet-ks <s.vineet.kumar@gmail.com>
2021-09-10 01:37:51 +0200

    Added write_over function. Uses separate image for bounddary and digits. get_corners returns img_persp and persp_mat, which is further used to create write_over.

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def getCorners(max_cnt):
     #print(len(approx))
     #print("-----")
     img_persp = None
+    persp_mat = None
     if len(approx) == 4:
         x2, y2, x1, y1, x3, y3, x4, y4 = approx.item(0), approx.item(1), approx.item(2), approx.item(3), approx.item(4), approx.item(5), approx.item(6), approx.item(7)
         boundary_pts = np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
@@ -31,9 +32,9 @@ def getCorners(max_cnt):
         new_boundary = np.float32([[0,0], [width, 0], [0, height], [width, height]])
 
         persp_mat = cv2.getPerspectiveTransform(boundary_pts, new_boundary)
-        img_persp = cv2.warpPerspective(img_processed, persp_mat, (width, height)) #Using eroded
+        img_persp = cv2.warpPerspective(im, persp_mat, (width, height)) #Using eroded
         #img_contours = cv2.cvtColor(img_persp, cv2.COLOR_BGR2GRAY)
-    return img_persp
+    return img_persp, persp_mat
 
 
 def grid_to_batch(cell_grid):
@@ -54,9 +55,22 @@ def predict_batch(cell_batch, empty_filter):
     predictions = predictions_raw * empty_filter
     return predictions
 
+def write_over(image):
+    print(type(image))
+    l, w, _ = image.shape
+    #print(l, w)
+    for i in range(9):
+        for j in range(9):
+            digit = str(int(predictions[i][j]))
+            if digit == '0':
+                continue
+            io = int(l * i / 9 + 27)
+            wo = int(w * j / 9 + 32)
+            image = cv2.putText(image, digit, (io, wo), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    return image
 
 vid = cv2.VideoCapture(0)
-model = load_model('model_mnist')
+model = load_model('model_temp')
 
 
 
@@ -75,11 +89,15 @@ while True:
     img_processed = thresh
     contours, _ = cv2.findContours(img_processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+    im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, im = cv2.threshold(im, 0, 255, cv2.THRESH_OTSU)
+    im = cv2.bitwise_not(im)
+
     max_contour = maxContour(contours)
     image_contours = None
     if max_contour is not None:
         cv2.drawContours(frame, max_contour, -1, (0, 0, 255), 2)
-        image_contours = getCorners(max_contour)
+        image_contours, persp_mat = getCorners(max_contour)
 
     #cv2.imshow('canny', canny)
     #cv2.imshow('dilated', dilated)
@@ -95,11 +113,16 @@ while True:
                 row.append(image_contours[5+i*38:33+i*38, 5+j*38:33+j*38])
             cell_grid.append(row)
 
-        cv2.imshow('cell1', cell_grid[0][1])
+        cv2.imshow('cell1', cell_grid[7][3])
 
         cell_batch, empty_filter = grid_to_batch(cell_grid)
         predictions = predict_batch(cell_batch, empty_filter)
-        print(predictions.reshape(9, 9))
+        predictions = predictions.reshape(9, 9)
+        print(predictions)
+
+        frame_persp = cv2.warpPerspective(frame, persp_mat, (342, 342))
+        image_write_over = write_over(frame_persp)
+        cv2.imshow("write_over", image_write_over)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
