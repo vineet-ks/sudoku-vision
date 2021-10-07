bdd9bb825bb9fc3f8c244089bd334412d7dbac82
vineet-ks <s.vineet.kumar@gmail.com>
2021-10-07 20:32:32 +0200

    Moved image processing for boundary and digits to functions and renamed variables

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ import cv2
 import numpy as np
 from keras.models import load_model
 
+
+def image_processing_boundary(image):
+    """Returns input image after processing for sudoku boundary detection"""
+    image = cv2.Canny(image, 50, 100)
+    image = cv2.GaussianBlur(image, (3, 3), 0)
+    kernel = np.ones((3, 3), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones((4, 4), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    _, image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
+    return image
+
+
+def image_processing_digits(image):
+    """Returns input image after processing for digit recognition"""
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    image = cv2.GaussianBlur(image, (3, 3), 0)
+    _, image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
+    image = cv2.bitwise_not(image)
+    return image
+
+
 def maxContour(contours):
     """Returns the contour with maximum area, if any"""
     max_area = 0
@@ -14,26 +36,28 @@ def maxContour(contours):
                 max_cnt = cnt_elt
     return max_cnt
 
+
 def getCorners(max_cnt):
     """Returns the edges of the contour if the approx polygon is a quadrilateral"""
     perimeter = cv2.arcLength(max_cnt, True)
     epsilon = 0.1 * perimeter
     approx = cv2.approxPolyDP(max_cnt, epsilon, True)
-    #print(approx)
-    #print(len(approx))
-    #print("-----")
+    # print(approx)
+    # print(len(approx))
+    # print("-----")
     img_persp = None
     persp_mat = None
     if len(approx) == 4:
-        x2, y2, x1, y1, x3, y3, x4, y4 = approx.item(0), approx.item(1), approx.item(2), approx.item(3), approx.item(4), approx.item(5), approx.item(6), approx.item(7)
+        x2, y2, x1, y1, x3, y3, x4, y4 = approx.item(0), approx.item(1), approx.item(2), approx.item(3), approx.item(
+            4), approx.item(5), approx.item(6), approx.item(7)
         boundary_pts = np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
 
         width, height = 342, 342
-        new_boundary = np.float32([[0,0], [width, 0], [0, height], [width, height]])
+        new_boundary = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
 
         persp_mat = cv2.getPerspectiveTransform(boundary_pts, new_boundary)
-        img_persp = cv2.warpPerspective(im, persp_mat, (width, height)) #Using eroded
-        #img_contours = cv2.cvtColor(img_persp, cv2.COLOR_BGR2GRAY)
+        img_persp = cv2.warpPerspective(img_processed_digits, persp_mat, (width, height))  # Using eroded
+        # img_contours = cv2.cvtColor(img_persp, cv2.COLOR_BGR2GRAY)
     return img_persp, persp_mat
 
 
@@ -45,7 +69,7 @@ def grid_to_batch(cell_grid):
         for j, cell in enumerate(row):
             cell_batch = np.append(cell_batch, cell.reshape(1, 28, 28, 1), axis=0)
             if np.sum(cell) < 15000:
-                empty_filter[i*9 + j] = 0
+                empty_filter[i * 9 + j] = 0
     return cell_batch, empty_filter
 
 
@@ -56,10 +80,11 @@ def predict_batch(cell_batch, empty_filter):
     predictions = predictions_raw * empty_filter
     return predictions
 
+
 def write_over(image):
     print(type(image))
     l, w, _ = image.shape
-    #print(l, w)
+    # print(l, w)
     for i in range(9):
         for j in range(9):
             digit = str(int(predictions[i][j]))
@@ -70,29 +95,17 @@ def write_over(image):
             image = cv2.putText(image, digit, (jo, io), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     return image
 
+
 vid = cv2.VideoCapture(0)
 model = load_model('model')
-
-
 
 while True:
     ret, frame = vid.read()
 
-    canny = cv2.Canny(frame, 50, 100)
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(canny, (3, 3), 0)
-    kernel = np.ones((3, 3), np.uint8)
-    dilated = cv2.dilate(blur, kernel, iterations=1)
-    kernel = np.ones((4, 4), np.uint8)
-    eroded = cv2.erode(dilated, kernel, iterations=1)
-    # blacks = np.zeros(frame.shape, dtype = np.uint8)
-    _, thresh = cv2.threshold(eroded, 0, 255, cv2.THRESH_OTSU)
-    img_processed = thresh
-    contours, _ = cv2.findContours(img_processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    img_processed_boundary = image_processing_boundary(frame)
+    contours, _ = cv2.findContours(img_processed_boundary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    _, im = cv2.threshold(im, 0, 255, cv2.THRESH_OTSU)
-    im = cv2.bitwise_not(im)
+    img_processed_digits = image_processing_digits(frame)
 
     max_contour = maxContour(contours)
     image_contours = None
@@ -100,9 +113,7 @@ while True:
         cv2.drawContours(frame, max_contour, -1, (0, 0, 255), 2)
         image_contours, persp_mat = getCorners(max_contour)
 
-    #cv2.imshow('canny', canny)
-    #cv2.imshow('dilated', dilated)
-    cv2.imshow('eroded', img_processed)
+    cv2.imshow('img_processed_boundary', img_processed_boundary)
     cv2.imshow('frame', frame)
     if image_contours is not None:
         cv2.imshow('image_contours', image_contours)
@@ -111,7 +122,7 @@ while True:
         for i in range(9):
             row = []
             for j in range(9):
-                row.append(image_contours[5+i*38:33+i*38, 5+j*38:33+j*38])
+                row.append(image_contours[5 + i * 38:33 + i * 38, 5 + j * 38:33 + j * 38])
             cell_grid.append(row)
 
         cv2.imshow('cell1', cell_grid[7][3])
